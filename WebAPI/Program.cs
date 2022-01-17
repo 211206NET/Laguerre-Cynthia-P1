f262ea5f7b46db9557159b1ac4ec40e0a280f0b3
Cynlaguerre <cynthia.laguerre@revature.net>
2022-01-17 12:56:34 -0500

    updating dependencies and json

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,3 +1,6 @@
+using DL;
+using BL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Registering our deps here for dependency injection
+builder.Services.AddScoped<IRepo>(ctx => new DBRepo(builder.Configuration.GetConnectionString("CYFDB")));
+builder.Services.AddScoped<IBL, CYFBL>();
 
 var app = builder.Build();
 
